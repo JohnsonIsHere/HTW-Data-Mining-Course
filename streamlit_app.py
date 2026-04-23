39d8e65a7f101554e618908e0cdc0423cd1248b7
JT Y. <163708145+JohnsonIsHere@users.noreply.github.com>
2026-04-23 07:40:09 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-print("Hello World")
+
+import streamlit as st
+
+st.title("🎈 My new app Tales")
+st.subheader("Hello")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
